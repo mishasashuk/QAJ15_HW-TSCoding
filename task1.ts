5ae0558053d26dc49c8f7e3bd91720f676cb0344
Alexandr Mishuk <mishasashuk@gmail.com>
2026-03-04 18:59:31 +0300

    fix: task1

--- task1.ts
+++ task1.ts
@@ -1,12 +1,13 @@
 // Создайте функцию, которая создает массив с 10 случайными числами и возвращает произведение 3 самых больших значений
 
-const arrayOfThree = () => {
+const arrayOfThree = (size: number = 10, maxValue: number = 100, multiply: number = 3) => {
   let arr = [];
-  for(let i = 0; i < 10; i++){
-  arr.push(Math.floor(Math.random() * 100) + 1);
+  for (let i = 0; i < size; i++) {
+    arr.push(Math.floor(Math.random() * maxValue) + 1);
   }
-  console.log(arr);
-  arr.sort((a, b) => b - a);
-  return arr[0] * arr[1] * arr[2];
-}
+  let resultArray = arr.splice(0, multiply);
+  const result = resultArray.reduce((acc, value) => acc * value);
+  // arr.sort((a, b) => b - a);
+  return result;
+};
 console.log(arrayOfThree());
