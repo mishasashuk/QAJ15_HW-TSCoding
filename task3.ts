64e8270db71693dabdf320c370bdecc27443dcda
Alexandr Mishuk <mishasashuk@gmail.com>
2026-03-13 05:46:03 +0300

    fix task3

--- task3.ts
+++ task3.ts
@@ -1,9 +1,12 @@
 // Создать функцию которая будет принимать входящее значение и добавлять к нему слово "супер"
 function addSuper(value: string) {
   const regexCyrillicLatin = /^[a-zA-ZА-Яа-яёЁ]+$/;
+  if (value === '') {
+    return 'суперкотик';
+  }
   if (!regexCyrillicLatin.test(value)) {
     throw new Error('Строка должна содержать только кириллицу или латиницу');
   }
   return `супер${value}`;
 }
-console.log(addSuper('котик'));
+console.log(addSuper('пес'));
