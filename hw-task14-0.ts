aec589d59d64e58d3b5b057c7451619667278397
Alexandr Mishuk <mishasashuk@gmail.com>
2026-03-16 23:37:31 +0300

    fix task14-0

--- hw-task14-0.ts
+++ hw-task14-0.ts
@@ -1,10 +1,13 @@
 // 1.  Создайте функцию, которая будет принимать в себя массив значений и возвращать только те, в которых заданное (второй параметр, по умолчанию - 4) количество букв
 function filterByLetter(arr: string[], letters: number = 4): string[] {
-  const result = arr.filter(item => item.length === letters);
+  const result = arr.filter(item => {
+    const lettersCount = (item.match(/[a-zA-Zа-яА-ЯёЁ]/g) || []).length;
+    return lettersCount === letters;
+  });
   if (result.length === 0) {
     throw new Error('Ничего не найдено');
   }
   return result;
 }
-const words = ['apple', '123', 'test1', '1test', 'test'];
+const words = ['apple', 'c00l', 'test1', '1test', 'test'];
 console.log(filterByLetter(words));
