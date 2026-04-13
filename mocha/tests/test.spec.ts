c3973574c8f783be00f787e61015c4e498ec2a22
Alexandr Mishuk <mishasashuk@gmail.com>
2026-04-13 17:51:01 +0300

    test calculator

--- mocha/tests/test.spec.ts
+++ mocha/tests/test.spec.ts
@@ -1,1 +1,76 @@
-import {expect} from 'chai';
+import { Calculator } from '../../hw-task14-1';
+import { expect } from 'chai';
+
+let calc: Calculator;
+
+beforeEach(() => {
+  calc = new Calculator();
+});
+
+describe('Test add function', () => {
+  it('two positive numbers', () => {
+    const result = calc.add(1, 2);
+    expect(result).equal(3);
+  });
+
+  it('multiple numbers', () => {
+    const result = calc.add(1, 2, 3);
+    expect(result).equal(6);
+  });
+
+  it('negative numbers', () => {
+    const result = calc.add(-2, -5);
+    expect(result).equal(-7);
+  });
+
+  it('positive and negative numbers', () => {
+    const result = calc.add(-3, 3);
+    expect(result).equal(0);
+  });
+
+  it('one number', () => {
+    const result = calc.add(5);
+    expect(result).equal(5);
+  });
+
+  it('decimal numbers', () => {
+    const result = calc.add(1.1, 2.4);
+    expect(result).equal(3.5);
+  });
+});
+
+describe('Test divide function', () => {
+  it('two positive numbers', () => {
+    const result = calc.divide(10, 2);
+    expect(result).equal(5);
+  });
+
+  it('multiple numbers', () => {
+    const result = calc.divide(12, 2, 3);
+    expect(result).equal(2);
+  });
+
+  it('negative numbers', () => {
+    const result = calc.divide(10, -2);
+    expect(result).equal(-5);
+  });
+
+  it('negative numbers', () => {
+    const result = calc.divide(10, -2);
+    expect(result).equal(-5);
+  });
+
+  it('one number', () => {
+    const result = calc.divide(5);
+    expect(result).equal(5);
+  });
+
+  it('decimal numbers', () => {
+    const result = calc.divide(2.4, 1.2);
+    expect(result).equal(2);
+  });
+
+  it('error when dividing by zero', () => {
+    expect(() => calc.divide(2, 0)).to.throw(Error, 'Dividing by zero is forbidden');
+  });
+});
